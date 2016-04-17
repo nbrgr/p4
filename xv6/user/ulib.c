39d7f36eaceeba9cd4e3a8a1b2b147b401a1276b
brgr-dev <berger1954@gmail.com>
2016-04-16 21:25:58 -0500

    added library functions (some of them)

--- xv6/user/ulib.c
+++ xv6/user/ulib.c
@@ -4,41 +4,6 @@
 #include "user.h"
 #include "x86.h"
 
-void lock_acquire(lock_t* lock)
-{
-  while(xchg(lock, 1) == 1);
-}
-
-void lock_release(lock_t* lock)
-{
-  xchg(lock, 0);
-}
-
-void lock_init(lock_t* lock)
-{
-  *lock = 0;
-}
-
-int thread_create(void (*start_routine)(void*), void *arg)
-{
-  void* stack = NULL;
-  stack = malloc(4096);
-  if (stack == NULL)
-    return -1;
-  return clone(start_routine, arg, stack);
-}
-
-int thread_join()
-{
-  void** stack = NULL;
-  int pid = 0;
-  pid = join(stack);
-  if (stack == NULL)
-    return -1;
-  free(stack);
-  return 0;
-}
-
 char*
 strcpy(char *s, char *t)
 {
