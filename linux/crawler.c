c52d3b7d1b3e473996b74afdd20a58729cca51c2
kbstoner <kstoner246@gmail.com>
2016-04-18 16:51:38 -0500

    Update crawler.c

--- linux/crawler.c
+++ linux/crawler.c
@@ -167,6 +167,7 @@ unsigned long hash(char *str)
 
 int hash_find_insert(hashtable *tbl, char* link) {
 	
+	printf("start insert\n");
         unsigned long key = hash(link) % (tbl->max);
         int found = 0;
         int insert = 0;
@@ -194,6 +195,7 @@ int hash_find_insert(hashtable *tbl, char* link) {
                         }
                 }
         }
+        printf("end insert");
         return found;
 }
 
