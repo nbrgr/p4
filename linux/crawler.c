5e9b31d858f621cac81e73205cb6f4b4dd16719b
kbstoner <kstoner246@gmail.com>
2016-04-18 23:46:04 -0500

    Update crawler.c

--- linux/crawler.c
+++ linux/crawler.c
@@ -201,11 +201,11 @@ int u_enqueue(struct u_queue* queue, char* url)
     }
     queue->size += 1;
     newnode->content = strcpy(newnode->content, url);
-    /*if(queue->size == 1) {
+    if(queue->size == 1) {
     	queue->front = newnode;
     }
     newnode->next = queue->back;
-    queue->back = newnode;*/
+    queue->back = newnode;
     return 0;
 }
 
