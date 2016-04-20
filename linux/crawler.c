5d94bdf216272f62dbc5e474b74023edda7929d6
kbstoner <kstoner246@gmail.com>
2016-04-19 19:37:39 -0500

    Update crawler.c

--- linux/crawler.c
+++ linux/crawler.c
@@ -347,7 +347,7 @@ void parse_page(char* page, void (*_edge_fn)(char *from, char *to))
 void downloader(char* (*_fetch_fn)(char *url))
 {
     printf("count: %i, complete: %i\n", work_count, work_completed);
-    while( !(b_isempty(download_queue) && u_isempty(parse_queue)) ) //work_count != work_completed)
+    while(work_count != work_completed)
     {
     	printf("count: %i, complete: %i\n", work_count, work_completed);
         pthread_mutex_lock(lock);
@@ -379,7 +379,7 @@ void parser(void (*_edge_fn)(char *from, char *to))
 {
     printf("parser begin\n");
     printf("count: %i, complete: %i\n", work_count, work_completed);
-    while( !(b_isempty(download_queue) && u_isempty(parse_queue)) ) {//work_count != work_completed) {
+    while(work_count != work_completed) {
     	printf("count: %i, complete: %i\n", work_count, work_completed);
         pthread_mutex_lock(lock);
         printf("start parser\n");
